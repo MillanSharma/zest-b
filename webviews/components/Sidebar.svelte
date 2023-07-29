e9231dbfde5f607527d87ae8cfbacc5a70e739ff
mukun_poudel <mukun@herm.is>
2023-07-29 18:31:32 +0530

    svelte basic

---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    let todos: Array<{text: string, completed: boolean}> = [];
+    let text = '';
+
+</script>
+<input />
+
+<form on:submit|preventDefault ={() =>{ 
+    todos = [...todos, { text: text, completed: false}];
+    text='';}
+}
+><input bind:value={text} /></form>
+
+<ul>{#each todos as todo (todo.text)}
+    <li>{todo.text}</li>
+    {/each}
+</ul>
